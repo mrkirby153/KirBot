49b50fad81586cd45568bb00642f617cf49e31d9
mrkirby153 <mr.austinwhyte@gmail.com>
2016-12-21 16:53:21 -0800

    Create bot configuration

---
+++ src/main/java/me/mrkirby153/KirBot/utils/BotConfiguration.java
@@ -0,0 +1,20 @@
+package me.mrkirby153.KirBot.utils;
+
+public class BotConfiguration {
+
+    /**
+     * The Discord id of the bot creator
+     */
+    public static final String MRKIRBY_ID = "117791909786812423";
+
+    /**
+     * The API key when interfacing with the
+     */
+    public String discordAPIKey = "";
+
+    /**
+     * The location where data is stored
+     */
+    public String dataLocation = "data";
+
+}

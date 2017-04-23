535861c4fd56f0366ff305cd54c4490e66f9ea67
mrkirby153 <mr.austinwhyte@gmail.com>
2017-04-23 13:50:38 -0700

    Notify users if real names are enforced

--- src/main/kotlin/me/mrkirby153/KirBot/EventListener.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/EventListener.kt
@@ -1,9 +1,15 @@
 package me.mrkirby153.KirBot
 
+import me.mrkirby153.KirBot.database.Database
+import me.mrkirby153.KirBot.realname.RealnameHandler
+import me.mrkirby153.KirBot.server.Server
 import me.mrkirby153.KirBot.server.ServerRepository
+import net.dv8tion.jda.core.entities.Member
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
+import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
+import net.dv8tion.jda.core.link
 
 class EventListener : ListenerAdapter() {
 
@@ -12,10 +18,26 @@ class EventListener : ListenerAdapter() {
         if (event!!.author == Bot.jda.selfUser)
             return
         val server = ServerRepository.getServer(event.guild) ?: return
-            server.handleMessageEvent(event)
+        server.handleMessageEvent(event)
     }
 
     override fun onGuildLeave(event: GuildLeaveEvent?) {
         ServerRepository.servers.remove(event?.guild?.id)
+    }
+
+    override fun onGuildMemberJoin(event: GuildMemberJoinEvent?) {
+        val server = ServerRepository.getServer(event!!.guild) ?: return
+        notifyUserOfRealName(event.member, server)
+    }
+
+    private fun notifyUserOfRealName(member: Member, server: Server) {
+        if (Database.requireRealname(server)) {
+            server.publicChannel.send().text("Welcome to **${server.name}** ${member.asMention}! We required you to set your real name.").queue {
+                server.publicChannel.send().embed("Real Name Required") {
+                    field("", true, "Click here to set your real name" link Bot.properties.getProperty("realname-url"))
+                }.rest().queue()
+            }
+            RealnameHandler(server).updateNames(true)
+        }
     }
 }

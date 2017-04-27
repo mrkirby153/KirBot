6c052090669a87e71a0682ea91087905a90f8e44
mrkirby153 <mr.austinwhyte@gmail.com>
2017-04-26 21:54:19 -0700

    Add support for changing volume by an amount
    Instead of setting the volume, prefixing the volume with "+" will increase the volume by that amount, "-" will decrease the volume

--- src/main/kotlin/me/mrkirby153/KirBot/command/executors/music/CommandVolume.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/executors/music/CommandVolume.kt
@@ -8,14 +8,34 @@ import net.dv8tion.jda.core.entities.Message
 class CommandVolume : MusicCommand() {
 
     override fun exec(message: Message, args: Array<String>) {
-        if(args.isEmpty()){
-            message.send().error("Please specify a volume!").queue()
+        val audioPlayer = server.musicManager.audioPlayer
+        if (args.isEmpty()) {
+            message.send().info("Current Volume: **${audioPlayer.volume}**").queue()
             return
         }
 
-        val volume = Math.min(150, Math.max(args[0].toInt(), 0))
+        var add = false
+        var sub = false
 
-        server.musicManager.audioPlayer.volume = volume
-        message.send().info("Set volume to __**$volume**__").queue()
+        var num = args[0]
+        if (num.startsWith("+")) {
+            num = num.substring(1)
+            add = true
+        } else if (num.startsWith("-")) {
+            num = num.substring(1)
+            sub = true
+        }
+
+        val volume = Math.min(150, Math.max(num.toInt(), 0))
+
+        if (add) {
+            audioPlayer.volume += volume
+        } else if (sub) {
+            audioPlayer.volume -= volume
+        } else {
+
+            audioPlayer.volume = volume
+        }
+        message.send().info("Set volume to __**${audioPlayer.volume}**__").queue()
     }
 }

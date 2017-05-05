9ac9e0c3028744a2b60b26e481fde8e7e53b7387
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-04 22:08:42 -0700

    Add embedded LaTeX commands
    LaTeX can be compiled by surrounding a segment of LaTeX with double dollar signs ($$)

---
+++ src/main/kotlin/me/mrkirby153/KirBot/command/processors/LaTeXProcessor.kt
@@ -0,0 +1,66 @@
+package me.mrkirby153.KirBot.command.processors
+
+import me.mrkirby153.KirBot.command.MessageProcessor
+import net.dv8tion.jda.core.MessageBuilder
+import net.dv8tion.jda.core.entities.Message
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.Charset
+
+class LaTeXProcessor : MessageProcessor("$$", "$$") {
+
+    val preamble = "\\usepackage{amsmath}\n\\usepackage{amsfonts}\n\\usepackage{amssymb}"
+
+    override fun process(message: Message) {
+        val msg = message.send().text("Processing LaTeX....").submit(true)
+        message.channel.sendTyping().submit(true)
+        for (match in matches) {
+            val url = getImageUrl(match)
+            val u = URL(url)
+            val con = u.openConnection() as HttpURLConnection
+            con.setRequestProperty("User-Agent", "KirBot")
+
+            val input = con.inputStream
+            val m = MessageBuilder().apply {
+                append(match)
+            }
+            message.channel.sendFile(input, "png", m.build()).queue()
+        }
+        message.channel.deleteMessageById(msg.get().id).queue()
+    }
+
+    fun getImageUrl(latex: String): String? {
+        val url = URL("http://quicklatex.com/latex3.f")
+        val con = url.openConnection() as HttpURLConnection
+        val postData = "formula=$latex&mode=0&out=1&fsize=45px&fcolor=737F8D&preamble=$preamble".toByteArray(Charset.defaultCharset())
+        con.requestMethod = "POST"
+        con.setRequestProperty("User-Agent", "KirBot")
+        con.setRequestProperty("Content-Type", "application/x-ww-form-urlencoded")
+        con.setRequestProperty("Content-Length", postData.size.toString())
+        con.setRequestProperty("charset", "utf-8")
+
+        con.doOutput = true
+
+        val os = con.outputStream
+        os.write(postData)
+        os.flush()
+        os.close()
+
+        val respCode = con.responseCode
+
+        if (respCode == HttpURLConnection.HTTP_OK) {
+            val `in` = BufferedReader(InputStreamReader(con.inputStream))
+            val resp = buildString {
+                while (true) {
+                    val value: String = `in`.readLine() ?: break
+                    if (value.startsWith("http:"))
+                        append(value)
+                }
+            }
+            return resp.split(" ")[0]
+        }
+        return null
+    }
+}

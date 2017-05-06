7c273d0c203dcee49d4b5be65284b725605e1f39
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-05 20:18:13 -0700

    Fix error in gitignore causing critical source files to be ignored

---
+++ src/main/kotlin/me/mrkirby153/KirBot/data/ServerData.kt
@@ -0,0 +1,40 @@
+package me.mrkirby153.KirBot.data
+
+import com.google.gson.GsonBuilder
+import me.mrkirby153.KirBot.Bot
+import me.mrkirby153.KirBot.Shard
+import me.mrkirby153.KirBot.music.MusicManager
+import me.mrkirby153.KirBot.server.ServerLogger
+import me.mrkirby153.KirBot.server.data.DataRepository
+import me.mrkirby153.KirBot.utils.child
+import me.mrkirby153.KirBot.utils.mkdirIfNotExist
+import java.nio.charset.Charset
+
+class ServerData(val id: Long, shard: Shard) {
+
+    private val guild = shard.getGuildById(id)
+
+
+    private val gson = with(GsonBuilder()) {
+        setPrettyPrinting()
+    }.create()
+
+    val repository: DataRepository by lazy {
+        val fileName = "$id.json"
+        val file = Bot.files.data.child("servers").mkdirIfNotExist().child(fileName)
+        if (!file.exists()) {
+            DataRepository(guild)
+        } else {
+            val reader = file.reader(Charset.defaultCharset())
+            val server = gson.fromJson(reader, DataRepository::class.java)
+            server.server = guild
+            reader.close()
+            server
+        }
+    }
+
+    val musicManager = MusicManager(guild)
+    val logger = ServerLogger(guild)
+
+
+}
---
+++ src/main/kotlin/me/mrkirby153/KirBot/server/data/DataRepository.kt
@@ -0,0 +1,37 @@
+package me.mrkirby153.KirBot.server.data
+
+import com.google.gson.GsonBuilder
+import me.mrkirby153.KirBot.Bot
+import me.mrkirby153.KirBot.utils.child
+import me.mrkirby153.KirBot.utils.mkdirIfNotExist
+import net.dv8tion.jda.core.entities.Guild
+
+private val GSON = GsonBuilder().setPrettyPrinting().create()
+
+class DataRepository(@Transient var server: Guild) {
+
+    private val data = mutableMapOf<String, String>()
+
+    fun <T> get(type: Class<T>, key: String): T? {
+        if (!data.containsKey(key))
+            return null
+        return GSON.fromJson(data[key], type)
+    }
+
+    fun put(key: String, value: Any) {
+        data.put(key, GSON.toJson(value))
+        save()
+    }
+
+    fun remove(key: String) {
+        data.remove(key)
+        save()
+    }
+
+    fun save() {
+        val fileName = "${server.id}.json"
+        val file = Bot.files.data.child("servers").mkdirIfNotExist().child(fileName)
+        val json = GSON.toJson(this)
+        file.writeText(json)
+    }
+}

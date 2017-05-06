ff7d4ec0b2fac3bbccc072439218d72f55783be2
mrkirby153 <mr.austinwhyte@gmail.com>
2017-05-06 10:33:28 -0700

    Reset audio manager if everyone leaves the voice chat

--- src/main/kotlin/me/mrkirby153/KirBot/listener/ShardListener.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/listener/ShardListener.kt
@@ -6,13 +6,14 @@ import me.mrkirby153.KirBot.command.CommandManager
 import me.mrkirby153.KirBot.database.Database
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 
-class ShardListener(val shard: Shard, val bot: Bot): ListenerAdapter() {
+class ShardListener(val shard: Shard, val bot: Bot) : ListenerAdapter() {
 
     override fun onMessageReceived(event: MessageReceivedEvent?) {
-        if(event!!.author == shard.selfUser)
+        if (event!!.author == shard.selfUser)
             return
         val serverData = shard.getServerData(event.guild)
 
@@ -25,5 +26,20 @@ class ShardListener(val shard: Shard, val bot: Bot): ListenerAdapter() {
 
     override fun onGuildJoin(event: GuildJoinEvent?) {
         Database.onJoin(event!!.guild)
+    }
+
+    override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent?) {
+        val guild = event!!.guild
+        // Ignore if KirBot isn't in that channel
+        if (guild.audioManager.connectedChannel != event.channelLeft) {
+            return
+        }
+
+        val usersInChannel = guild.audioManager.connectedChannel.members.filter { it.id != guild.selfMember.id }.size
+
+        if(usersInChannel < 1){
+            shard.getServerData(guild).musicManager.trackScheduler.reset()
+        }
+
     }
 }

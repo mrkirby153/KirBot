5d4d4034a533609fc0046749b1e6c8a2bb52a835
mrkirby153 <mr.austinwhyte@gmail.com>
2017-12-10 14:42:33 -0800

    Updated how messages are stored in the redis queue

--- src/main/kotlin/me/mrkirby153/KirBot/redis/messaging/MessageDataStore.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/redis/messaging/MessageDataStore.kt
@@ -9,74 +9,83 @@ class MessageDataStore {
 
     private val LIST_KEY = "messages"
 
+    private val DATA_KEY_PREFIX = "message"
+
     fun getMessageContent(id: String, callback: (msg: Message?) -> Unit) {
         RedisConnector.get().use {
-            val listSize = it.llen(LIST_KEY)
-
-            val list = it.lrange(LIST_KEY, 0, listSize).map { JSONObject(JSONTokener(it)) }.map { decode(it) }
-
-            val messages = list.filter { it.id == id }
-
-            if (messages.isEmpty()) {
+            val msg = getMessageFromRedis(id)
+            if (msg != null)
+                callback.invoke(msg)
+            else {
                 ServerMessage.get(id).queue {
-                    callback.invoke(Message(it.id ?: "-1", it.serverId, it.authorId, it.channelId, it.content))
+                    callback.invoke(Message(it.id ?: "-1", it.serverId, it.authorId, it.channelId,
+                            it.content))
                 }
-                return
             }
+        }
+    }
 
-            callback.invoke(messages[messages.lastIndex])
+    fun storeMessage(message: net.dv8tion.jda.core.entities.Message): String {
+        val key = "$DATA_KEY_PREFIX:${message.id}"
+        RedisConnector.get().use {
+            it.set(key, encode(Message(message)).toString())
+            return key
+        }
+    }
+
+    fun getMessageFromRedis(key: String): Message? {
+        RedisConnector.get().use {
+            val json = it.get("$DATA_KEY_PREFIX:$key") ?: return null
+            val obj = JSONObject(JSONTokener(json))
+            return decode(obj)
         }
     }
 
     fun pushMessage(message: net.dv8tion.jda.core.entities.Message) {
         RedisConnector.get().use {
-            val json = encode(Message(message.id, message.guild.id, message.author.id, message.channel.id, message.content, message.creationTime.toEpochSecond()))
-            it.rpush(LIST_KEY, json.toString())
+            it.rpush(LIST_KEY, storeMessage(message))
         }
     }
 
     fun deleteMessage(messageId: String, callback: ((msg: String) -> Unit)?) {
-        RedisConnector.get().use {
-            val msgs = it.llen(LIST_KEY)
-            val msgList = it.lrange(LIST_KEY, 0, msgs)
-            msgList.forEach { msg ->
-                val decoded = decode(JSONObject(JSONTokener(msg)))
-                if (decoded.id == messageId) {
-                    callback?.invoke(decoded.message)
-                    it.lrem(LIST_KEY, 0, msg)
-                    return
-                }
+        val msg = getMessageFromRedis(messageId)
+        if (msg != null) { // Message is queued in redis
+            RedisConnector.get().use {
+                it.lrem(LIST_KEY, 0, "$DATA_KEY_PREFIX:$messageId")
+                it.del("$DATA_KEY_PREFIX:$messageId")
+                callback?.invoke(msg.message)
+                return
             }
-        }
-        ServerMessage.delete(messageId).queue {
-            callback?.invoke(it.content)
+        } else {
+            ServerMessage.delete(messageId).queue {
+                callback?.invoke(it.content)
+            }
         }
     }
 
     fun bulkDelete(messageIds: Array<String>, callback: (() -> Unit)?) {
         val deleted = mutableListOf<String>()
         RedisConnector.get().use {
-            val msgList = it.lrange(LIST_KEY, 0, it.llen(LIST_KEY))
-            msgList.forEach { msg ->
-                val decoded = decode(JSONObject(JSONTokener(msg)))
-                deleted.add(decoded.id)
-                if (decoded.id in messageIds)
-                    it.lrem(LIST_KEY, 0, msg)
+            messageIds.forEach { id ->
+                if (getMessageFromRedis(id) != null) {
+                    it.lrem(LIST_KEY, 0, "$DATA_KEY_PREFIX:$id")
+                    it.del("$DATA_KEY_PREFIX:$id")
+                    deleted.add(id)
+                }
             }
         }
-        val toDelete = messageIds.clone().toMutableList()
-        toDelete.removeAll(deleted)
-        println(toDelete)
-        if (toDelete.isNotEmpty())
+        val toDelete = messageIds.clone().filter { it !in deleted }
+        if (toDelete.isNotEmpty()) {
             ServerMessage.bulkDelete(toDelete).queue {
                 callback?.invoke()
             }
-        else
+        } else {
             callback?.invoke()
+        }
     }
 
 
-    fun encode(message: Message): JSONObject = JSONObject().apply {
+    private fun encode(message: Message): JSONObject = JSONObject().apply {
         put("id", message.id)
         put("author", message.author)
         put("channel", message.channel)
@@ -85,8 +94,15 @@ class MessageDataStore {
         put("time", message.time)
     }
 
-    fun decode(obj: JSONObject): Message = Message(obj.getString("id"), obj.getString("server"), obj.getString("author"), obj.getString("channel"), obj.getString("message"), obj.optLong("time"))
+    private fun decode(obj: JSONObject): Message = Message(obj.getString("id"), obj.getString("server"),
+            obj.getString("author"), obj.getString("channel"), obj.getString("message"),
+            obj.optLong("time"))
 
 
-    data class Message(val id: String, val serverId: String, val author: String, val channel: String, val message: String, val time: Long? = System.currentTimeMillis())
+    data class Message(val id: String, val serverId: String, val author: String,
+                       val channel: String, val message: String,
+                       val time: Long? = System.currentTimeMillis()) {
+        constructor(msg: net.dv8tion.jda.core.entities.Message) : this(msg.id, msg.guild.id,
+                msg.author.id, msg.channel.id, msg.content, msg.creationTime.toEpochSecond())
+    }
 }

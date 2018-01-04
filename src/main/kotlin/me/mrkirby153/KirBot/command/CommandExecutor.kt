64403ef9ab533bddfd31fb9b643842df325859e3
mrkirby153 <mr.austinwhyte@gmail.com>
2018-01-04 11:27:46 -0800

    Execute commands on their own thread

--- src/main/kotlin/me/mrkirby153/KirBot/command/CommandExecutor.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/CommandExecutor.kt
@@ -1,5 +1,6 @@
 package me.mrkirby153.KirBot.command
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import me.mrkirby153.KirBot.Bot
 import me.mrkirby153.KirBot.Shard
 import me.mrkirby153.KirBot.command.args.ArgumentParseException
@@ -16,6 +17,7 @@ import net.dv8tion.jda.core.entities.Channel
 import net.dv8tion.jda.core.entities.ChannelType
 import net.dv8tion.jda.core.entities.Guild
 import org.reflections.Reflections
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
 
@@ -24,6 +26,9 @@ object CommandExecutor {
     val commands = mutableListOf<CommandSpec>()
 
     val helpManager = HelpManager()
+
+    private val executorThread = Executors.newFixedThreadPool(2,
+            ThreadFactoryBuilder().setDaemon(true).setNameFormat("KirBot Command Executor-%d").build())
 
     fun loadAll() {
         Bot.LOG.info("Starting loading of commands, this may take a while")
@@ -41,99 +46,106 @@ object CommandExecutor {
     }
 
     fun execute(context: Context) {
-        if (context.channel.type == ChannelType.PRIVATE) {
-            return
-        }
-
-        val shard = context.shard
-        val guild = context.guild
-
-        var message = context.rawContent
-
-        if (message.isEmpty())
-            return
-
-        val settings = shard.serverSettings[guild.id] ?: return
-
-        val prefix = settings.cmdDiscriminator
-
-        if (!message.startsWith(prefix)) {
-            return
-        }
-
-        Bot.LOG.debug("Processing message \"$message\"")
-
-        message = message.substring(prefix.length)
-
-        val parts = message.split(" ").filter { it.isNotEmpty() }.map { it.trim() }.toTypedArray()
-
-        val cmd = parts[0].toLowerCase()
-
-        Bot.LOG.debug("Looking up $cmd on ${guild.id}")
-
-        val args = if (parts.isNotEmpty()) parts.drop(1).toTypedArray() else emptyArray()
-
-        val command = getCommand(cmd)
-        if (command == null) {
-            executeCustomCommand(context, cmd, args, shard, guild)
-            return
-        }
-
-        if (!canExecuteInChannel(command, context.channel as Channel)) {
-            Bot.LOG.debug("Ignoring command because of channel whitelist")
-            return
-        }
-
-        if (command.clearance.value > context.author.getClearance(guild).value) {
-            Bot.LOG.debug("${context.author.id} was denied access to $cmd due to lack of clearance. Required: ${command.clearance}, Found: ${context.author.getClearance(guild)}")
-            context.send().error("You do not have permission to perform this command!").queue {
-                it.deleteAfter(10, TimeUnit.SECONDS)
-                context.deleteAfter(10, TimeUnit.SECONDS)
+        this.executorThread.submit({
+            if (context.channel.type == ChannelType.PRIVATE) {
+                return@submit
             }
-            return
-        }
 
-        Bot.LOG.debug("Beginning parsing of arguments: [${args.joinToString(",")}]")
+            val shard = context.shard
+            val guild = context.guild
 
-        val parser = ArgumentParser(args)
+            var message = context.rawContent
 
-        val cmdContext: CommandContext
-        try {
-            cmdContext = parser.parse(command.arguments.toTypedArray())
-        } catch (e: ArgumentParseException) {
-            context.send().error(e.message ?: "Invalid argument format!").queue {
-                it.deleteAfter(10, TimeUnit.SECONDS)
-                context.deleteAfter(10, TimeUnit.SECONDS)
+            if (message.isEmpty())
+                return@submit
+
+            val settings = shard.serverSettings[guild.id] ?: return@submit
+
+            val prefix = settings.cmdDiscriminator
+
+            if (!message.startsWith(prefix)) {
+                return@submit
             }
-            return
-        }
 
-        Bot.LOG.debug("Parsed: $cmdContext")
+            Bot.LOG.debug("Processing message \"$message\"")
 
-        Bot.LOG.debug("Executing command: $cmd")
+            message = message.substring(prefix.length)
 
-        try {
-            val executor = command.executor
-            executor.aliasUsed = cmd
-            executor.cmdPrefix = prefix
-            executor.execute(context, cmdContext)
-        } catch (e: CommandException) {
-            context.send().error(e.message ?: "An unknown error has occurred!").queue {
-                context.deleteAfter(10, TimeUnit.SECONDS)
-                it.deleteAfter(10, TimeUnit.SECONDS)
+            val parts = message.split(" ").filter { it.isNotEmpty() }.map { it.trim() }.toTypedArray()
+
+            val cmd = parts[0].toLowerCase()
+
+            Bot.LOG.debug("Looking up $cmd on ${guild.id}")
+
+            val args = if (parts.isNotEmpty()) parts.drop(1).toTypedArray() else emptyArray()
+
+            val command = getCommand(cmd)
+            if (command == null) {
+                executeCustomCommand(context, cmd, args, shard, guild)
+                return@submit
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            context.send().error("An unknown error has occurred, please try again").queue {
-                it.deleteAfter(10, TimeUnit.SECONDS)
-                context.deleteAfter(10, TimeUnit.SECONDS)
+
+            if (!canExecuteInChannel(command, context.channel as Channel)) {
+                Bot.LOG.debug("Ignoring command because of channel whitelist")
+                return@submit
             }
-        }
-        Bot.LOG.debug("Command execution finished")
+
+            if (command.clearance.value > context.author.getClearance(guild).value) {
+                Bot.LOG.debug(
+                        "${context.author.id} was denied access to $cmd due to lack of clearance. Required: ${command.clearance}, Found: ${context.author.getClearance(
+                                guild)}")
+                context.send().error("You do not have permission to perform this command!").queue {
+                    it.deleteAfter(10, TimeUnit.SECONDS)
+                    context.deleteAfter(10, TimeUnit.SECONDS)
+                }
+                return@submit
+            }
+
+            Bot.LOG.debug("Beginning parsing of arguments: [${args.joinToString(",")}]")
+
+            val parser = ArgumentParser(args)
+
+            val cmdContext: CommandContext
+            try {
+                cmdContext = parser.parse(command.arguments.toTypedArray())
+            } catch (e: ArgumentParseException) {
+                context.send().error(e.message ?: "Invalid argument format!").queue {
+                    it.deleteAfter(10, TimeUnit.SECONDS)
+                    context.deleteAfter(10, TimeUnit.SECONDS)
+                }
+                return@submit
+            }
+
+            Bot.LOG.debug("Parsed: $cmdContext")
+
+            Bot.LOG.debug("Executing command: $cmd")
+
+            try {
+                val executor = command.executor
+                executor.aliasUsed = cmd
+                executor.cmdPrefix = prefix
+                executor.execute(context, cmdContext)
+            } catch (e: CommandException) {
+                context.send().error(e.message ?: "An unknown error has occurred!").queue {
+                    context.deleteAfter(10, TimeUnit.SECONDS)
+                    it.deleteAfter(10, TimeUnit.SECONDS)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                context.send().error("An unknown error has occurred, please try again").queue {
+                    it.deleteAfter(10, TimeUnit.SECONDS)
+                    context.deleteAfter(10, TimeUnit.SECONDS)
+                }
+            }
+            Bot.LOG.debug("Command execution finished")
+        })
     }
 
-    fun executeCustomCommand(context: Context, command: String, args: Array<String>, shard: Shard, guild: Guild) {
-        val customCommand = shard.customCommands[guild.id]?.obj?.firstOrNull { it.name.equals(command, true) } ?: return
+    fun executeCustomCommand(context: Context, command: String, args: Array<String>, shard: Shard,
+                             guild: Guild) {
+        val customCommand = shard.customCommands[guild.id]?.obj?.firstOrNull {
+            it.name.equals(command, true)
+        } ?: return
         if (customCommand.clearance.value > context.author.getClearance(guild).value) {
             context.send().error("You do not have permission to perform that command").queue {
                 context.deleteAfter(10, TimeUnit.SECONDS)
@@ -141,13 +153,13 @@ object CommandExecutor {
             }
             return
         }
-        if (!canExecuteInChannel(customCommand, context.channel as Channel)){
+        if (!canExecuteInChannel(customCommand, context.channel as Channel)) {
             return
         }
 
         var response = customCommand.data
-        for(i in 0 until args.size){
-            response = response.replace("%${i+1}", args[i])
+        for (i in 0 until args.size) {
+            response = response.replace("%${i + 1}", args[i])
         }
 
         context.channel.sendMessage(response).queue()
@@ -174,22 +186,25 @@ object CommandExecutor {
         }
     }
 
-    fun getCommandsByCategory() : Map<CommandCategory, List<CommandSpec>> {
+    fun getCommandsByCategory(): Map<CommandCategory, List<CommandSpec>> {
         val categories = mutableMapOf<CommandCategory, MutableList<CommandSpec>>()
         this.commands.forEach {
-            if(!categories.containsKey(it.category))
+            if (!categories.containsKey(it.category))
                 categories[it.category] = mutableListOf()
             categories[it.category]?.add(it)
         }
         return categories
     }
 
-    private fun getCommand(name: String) = this.commands.firstOrNull { it.aliases.map { it.toLowerCase() }.contains(name.toLowerCase()) }
+    private fun getCommand(name: String) = this.commands.firstOrNull {
+        it.aliases.map { it.toLowerCase() }.contains(name.toLowerCase())
+    }
 
     private fun canExecuteInChannel(command: CommandSpec, channel: Channel): Boolean {
-        val data = Bot.getShardForGuild(channel.guild.id)?.serverSettings?.get(channel.guild.id) ?: return false
+        val data = Bot.getShardForGuild(channel.guild.id)?.serverSettings?.get(
+                channel.guild.id) ?: return false
         return if (command.respectWhitelist) {
-            if(data.whitelistedChannels.isEmpty())
+            if (data.whitelistedChannels.isEmpty())
                 return true
             data.whitelistedChannels.any { it == channel.id }
         } else {
@@ -198,17 +213,21 @@ object CommandExecutor {
     }
 
     private fun canExecuteInChannel(command: GuildCommand, channel: Channel): Boolean {
-        val data = Bot.getShardForGuild(channel.guild.id)?.serverSettings?.get(channel.guild.id) ?: return false
+        val data = Bot.getShardForGuild(channel.guild.id)?.serverSettings?.get(
+                channel.guild.id) ?: return false
         return if (command.respectWhitelist) {
-            if(data.whitelistedChannels.isEmpty())
-               return true
+            if (data.whitelistedChannels.isEmpty())
+                return true
             data.whitelistedChannels.any { it == channel.id }
         } else {
             true
         }
     }
 
-    private fun getEffectivePermission(command: String, guild: Guild, default: Clearance): Clearance {
-        return Bot.getShardForGuild(guild.id)!!.clearanceOverrides[guild.id]?.obj?.firstOrNull { it.command.equals(command, true) }?.clearance ?: default
+    private fun getEffectivePermission(command: String, guild: Guild,
+                                       default: Clearance): Clearance {
+        return Bot.getShardForGuild(guild.id)!!.clearanceOverrides[guild.id]?.obj?.firstOrNull {
+            it.command.equals(command, true)
+        }?.clearance ?: default
     }
 }

4b44595b989b2e6e062d3b43a868365a1338c3c0
mrkirby153 <mr.austinwhyte@gmail.com>
2018-02-01 17:06:54 -0800

    Show how to use the quote system when an id isn't passed

--- src/main/kotlin/me/mrkirby153/KirBot/command/executors/fun/CommandQuote.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/executors/fun/CommandQuote.kt
@@ -13,8 +13,14 @@ import me.mrkirby153.KirBot.utils.botUrl
 import me.mrkirby153.KirBot.utils.escapeMentions
 
 @Command("quote")
-class CommandQuote : BaseCommand(false, CommandCategory.FUN, Arguments.number("id")) {
+class CommandQuote : BaseCommand(false, CommandCategory.FUN, Arguments.number("id", false)) {
     override fun execute(context: Context, cmdContext: CommandContext) {
+        if (!cmdContext.has("id")) {
+            context.channel.sendMessage(
+                    ":left_speech_bubble: **Quotes**\n\n React to any message with :left_speech_bubble: " +
+                            "to quote it. You can retrieve a quote by its id by typing `${cmdPrefix}quote <id>`. To view a full list of quotes, type `${cmdPrefix}quotes`").queue()
+            return
+        }
         val quote = cmdContext.get<Double>("id")
         val q = Model.first(Quote::class.java,
                 quote.toString()) ?: throw CommandException("That quote doesn't exist")

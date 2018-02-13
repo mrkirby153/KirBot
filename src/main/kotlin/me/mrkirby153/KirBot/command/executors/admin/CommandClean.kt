720d97e678f520b3a2339e295f6887593de04304
mrkirby153 <mr.austinwhyte@gmail.com>
2018-02-13 15:30:05 -0800

    Fix the clean command again

--- src/main/kotlin/me/mrkirby153/KirBot/command/executors/admin/CommandClean.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/executors/admin/CommandClean.kt
@@ -1,6 +1,5 @@
 package me.mrkirby153.KirBot.command.executors.admin
 
-import me.mrkirby153.KirBot.Bot
 import me.mrkirby153.KirBot.command.BaseCommand
 import me.mrkirby153.KirBot.command.Command
 import me.mrkirby153.KirBot.command.CommandCategory
@@ -14,6 +13,7 @@ import me.mrkirby153.KirBot.utils.checkPermissions
 import me.mrkirby153.KirBot.utils.deleteAfter
 import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.TextChannel
+import net.dv8tion.jda.core.entities.User
 import java.time.OffsetDateTime
 import java.util.concurrent.TimeUnit
 
@@ -36,56 +36,50 @@ class CommandClean :
             var amountLeft = amount // The amount of messages we need to delete
 
             val waitMessage = channel.sendMessage(
-                    ":arrows_counterclockwise: Please wait while I delete up to $amount messages...").complete()
+                    ":arrows_counterclockwise: Please wait while I delete up to $amount messages" + buildString {
+                        if (cmdContext.has("user")) {
+                            val user = cmdContext.get<User>("user")
+                            append(" from `" + user!!.name + "#" + user.discriminator + "`")
+                        }
+                    } + "...").complete()
             var deletedMessages = 0
+            val history = channel.history
             while (amountLeft > 0) {
-                val amountToDelete = Math.min(amountLeft, 100) // Delete 100 max
-                amountLeft -= amountToDelete
-                Bot.LOG.debug("Attempting to delete $amountToDelete messages")
-                channel.history.retrievePast(amountToDelete).queue { messages ->
-                    val m = messages.filter {
-                        // We can only delete messages in the last two weeks
-                        it.creationTime.isAfter(OffsetDateTime.now().minusDays(14))
-                    }.filter {
-                                // Lets not delete the waiting message
-                                it.id != waitMessage.id
-                            }.filter {
-                                // A user was passed in
-                                if (cmdContext.has("user")) {
-                                    it.author == cmdContext.get("user")
-                                } else {
-                                    true
-                                }
-                            }
+                val messages = history.retrievePast(100).complete().filter {
+                    it.creationTime.isAfter(
+                            OffsetDateTime.now().minusDays(
+                                    14)) // We can only delete messages 14 days old
+                }.filter {
+                            it.id != waitMessage.id // We don't want to delete the wait message
+                        }
+                // Filter these messages for matching query
+                val filtered = messages.filter {
+                    if (cmdContext.has("user")) {
+                        it.author == cmdContext.get("user")
+                    } else {
+                        true
+                    }
+                }
+                if (filtered.isEmpty()) {
+                    // We found no messages, we're done here
+                    break
+                }
 
-                    // We didn't retrieve any more messages
-                    if (m.isEmpty()) {
-                        // We're done, there are no messages left to delete
-                        waitMessage.editMessage(
-                                ":ok_hand: Deleted $deletedMessages messages").queue {
-                            it.deleteAfter(30, TimeUnit.SECONDS)
-                        }
-                        return@queue
-                    }
-                    // Increment the messages we've deleted
-                    deletedMessages += m.size
-                    if (m.size == 1) {
-                        // We can't bulk delete two messages, so lets just delete the first one
-                        m[0].delete().queue()
-                        waitMessage.editMessage(
-                                ":ok_hand: Deleted $deletedMessages messages").queue {
-                            it.deleteAfter(30, TimeUnit.SECONDS)
-                        }
-                    }
-                    // Delete all the messages
-                    channel.deleteMessages(m).queue {
-                        if (amountLeft <= 0) {
-                            waitMessage.editMessage(
-                                    ":ok_hand: Deleted $deletedMessages messages").queue {
-                                it.deleteAfter(30, TimeUnit.SECONDS)
-                            }
-                        }
-                    }
+                // Of the remaining, we need to retrieve max the amount to delete
+                val toDelete = filtered.subList(0,
+                        Math.min(Math.min(amountLeft, filtered.size), 99))
+
+                amountLeft -= toDelete.size
+                deletedMessages += toDelete.size
+
+                if (toDelete.size == 1) {
+                    // Just delete the first message cos we can't bulk delete it
+                    toDelete[0].delete().queue()
+                } else {
+                    channel.deleteMessages(toDelete).queue()
+                }
+                waitMessage.editMessage(":ok_hand: Deleted $deletedMessages messages").queue {
+                    it.deleteAfter(10, TimeUnit.SECONDS)
                 }
             }
         } else {

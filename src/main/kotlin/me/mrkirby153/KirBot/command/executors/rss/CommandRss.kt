ef7f8139899d201eb4cdf59283546cc562ca933d
mrkirby153 <mr.austinwhyte@gmail.com>
2018-02-25 12:33:01 -0800

    Move RSS command to the sub-command system

--- src/main/kotlin/me/mrkirby153/KirBot/command/executors/rss/CommandRss.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/executors/rss/CommandRss.kt
@@ -14,113 +14,123 @@ import me.mrkirby153.KirBot.database.models.rss.RssFeed
 import me.mrkirby153.KirBot.rss.FeedTask
 import me.mrkirby153.KirBot.user.Clearance
 import me.mrkirby153.KirBot.utils.Context
+import me.mrkirby153.KirBot.utils.HttpUtils
 import me.mrkirby153.kcutils.Time
+import okhttp3.Request
 import java.net.URL
 import java.util.concurrent.TimeUnit
 
-@Command(name = "rss", arguments = ["[action:string]", "[parameters:string...]"], clearance = Clearance.BOT_MANAGER)
-class CommandRss :
-        BaseCommand(false, CommandCategory.MISCELLANEOUS) {
+@Command(name = "rss", clearance = Clearance.BOT_MANAGER)
+class CommandRss : BaseCommand(false, CommandCategory.MISCELLANEOUS) {
 
     override fun execute(context: Context, cmdContext: CommandContext) {
-        val action = cmdContext.get<String>("action")?.toLowerCase()
-        val parameters: List<String> = cmdContext.get<String>("parameters")?.split(
-                " ")?.filter { it.isNotEmpty() } ?: emptyList()
-        if (action == null || action == "list") {
-            // Display feeds in this channel
-            val feeds = Model.get(RssFeed::class.java,
-                    Pair("server_id", context.guild.id))
-            context.send().embed("RSS Feeds") {
-                description {
-                    +"The following RSS Feeds are registered on this channel: \n\n"
-                    feeds.filter { it.channelId == context.channel.id }.forEach {
-                        +"`${it.id}`"
-                        +" - "
-                        +it.url
-                        if (!it.failed) {
-                            +" (Last Update: "
-                            if (it.lastCheck != null) {
-                                +Time.format(1, System.currentTimeMillis() - it.lastCheck!!.time)
-                                +" Ago"
-                            } else
-                                +"Never"
-                            +")"
+        listFeeds(context, cmdContext)
+    }
+
+    @Command(name = "list", clearance = Clearance.BOT_MANAGER)
+    fun listFeeds(context: Context, cmdContext: CommandContext) {
+        val feeds = Model.get(RssFeed::class.java, Pair("server_id", context.guild.id))
+
+        context.send().embed("RSS Feeds") {
+            description {
+                +"This channel is subscribed to the following feeds: \n\n"
+                feeds.filter { it.channelId == context.channel.id }.forEach {
+                    +"`${it.id}`"
+                    +" - "
+                    +it.url
+                    if (!it.failed) {
+                        +" (Last Update: "
+                        if (it.lastCheck != null) {
+                            +Time.format(1, System.currentTimeMillis() - it.lastCheck!!.time)
+                            +" Ago"
                         } else {
-                            +" (Failed, must be refreshed manually)"
+                            +"Never"
                         }
-                        +"\n"
+                        +")"
+                    } else {
+                        +" (Failed. Must be refreshed manually)"
                     }
-                    +"\n\n"
-                    +"Use `${cmdPrefix}rss remove <id>` to delete the feed\n"
-                    +"Use `${cmdPrefix}rss add <url>` to add a feed\n"
-                    +"Use `${cmdPrefix}rss refresh <id>` to force a refresh"
+                    +"\n"
                 }
-            }.rest().queue()
-            return
+                +"\n\n"
+                +"Use `${cmdPrefix}rss remove <id>` to delete the feed\n"
+                +"Use `${cmdPrefix}rss add <url>` to add a feed\n"
+                +"Use `${cmdPrefix}rss refresh <id>` to refresh the feed now"
+            }
+        }.rest().queue()
+    }
+
+    @Command(name = "add", arguments = ["<url:string>"], clearance = Clearance.BOT_MANAGER)
+    fun addFeed(context: Context, cmdContext: CommandContext) {
+        val url = cmdContext.get<String>("url") ?: throw CommandException("Please provide a URL")
+
+        // Check if the URL actually exists
+        val req = Request.Builder().apply {
+            url(url)
+        }.build()
+
+        val resp = HttpUtils.CLIENT.newCall(req).execute()
+
+        if (resp.code() != 200) {
+            throw CommandException(
+                    "The URL you provided is invalid. (HTTP ${resp.code()} returned)")
         }
 
-        when (action) {
-            "add" -> {
-                if (parameters.isEmpty())
-                    throw CommandException("Please specify a URL")
-                val rssFeed = parameters[0]
-                // Make a new RSS feed
-                val feed = RssFeed()
-                feed.id = Model.randomId()
-                feed.channelId = context.channel.id
-                feed.serverId = context.guild.id
-                feed.url = rssFeed
-                feed.save()
-                // Load new feeds async
-                Bot.scheduler.schedule({
-                    try {
-                        val url = URL(feed.url)
+        // Create the feed
+        val feed = RssFeed()
+        feed.id = Model.randomId()
+        feed.channelId = context.channel.id
+        feed.serverId = context.guild.id
+        feed.url = url
+        feed.save()
+        // Load all the posts so we don't spam with new posts
+        Bot.scheduler.schedule({
+            try {
+                val feedUrl = URL(url)
 
-                        val input = SyndFeedInput()
-                        val f = input.build(XmlReader(url))
-                        f.entries.forEach {
-                            val item = FeedItem()
-                            item.id = Model.randomId()
-                            item.feedId = feed.id
-                            item.guid = it.uri
-                            item.save()
-                        }
-                    } catch (e: Exception) {
-                        // Ignore
-                    }
-                }, 0, TimeUnit.MILLISECONDS)
-                context.send().success(
-                        "Feed has been registered, new posts will be posted here").queue()
-            }
-            "remove" -> {
-                if (parameters.isEmpty())
-                    throw CommandException("Please specify an ID")
-                // TODO 12/15/2017 Check if the RSS feed actually exists
-                val feed = Model.first(RssFeed::class.java,
-                        parameters[0]) ?: throw CommandException("That feed does not exist")
-                feed.delete()
-                context.send().success("Deleted RSS feed!").queue {
-                    it.delete().queueAfter(10, TimeUnit.SECONDS)
-                    context.message.delete().queueAfter(10, TimeUnit.SECONDS)
+                val input = SyndFeedInput()
+                val f = input.build(XmlReader(feedUrl))
+                f.entries.forEach {
+                    val item = FeedItem()
+                    item.id = Model.randomId()
+                    item.feedId = feed.id
+                    item.guid = it.uri
+                    item.save()
                 }
+            } catch (e: Exception) {
+                // Ignore
             }
-            "refresh" -> {
-                if (parameters.isEmpty()) {
-                    FeedTask.checkFeeds(context.guild)
-                    context.send().success("Refreshed RSS feeds").queue {
-                        it.delete().queueAfter(10, TimeUnit.SECONDS)
-                        context.message.delete().queueAfter(10, TimeUnit.SECONDS)
-                    }
-                } else {
-                    val feed = Model.first(RssFeed::class.java,
-                            parameters[0]) ?: throw CommandException("That feed doesn't exist")
-                    FeedTask.checkFeed(feed)
-                    context.send().success("Refreshed RSS feed").queue {
-                        it.delete().queueAfter(10, TimeUnit.SECONDS)
-                        context.message.delete().queueAfter(10, TimeUnit.SECONDS)
-                    }
-                }
-            }
+        }, 0, TimeUnit.MILLISECONDS)
+        context.send().success("Feed has been registered, new posts will appear here").queue()
+    }
+
+    @Command(name = "remove", arguments = ["<id:string>"], clearance = Clearance.BOT_MANAGER)
+    fun removeFeed(context: Context, cmdContext: CommandContext) {
+        val id = cmdContext.get<String>("id") ?: throw CommandException("Please provide a feed Id")
+
+        val feed = Model.first(RssFeed::class.java, id)
+        if (feed == null || feed.serverId != context.guild.id)
+            throw CommandException("That feed does not exist")
+        feed.delete()
+        context.send().success("Deleted RSS Feed!").queue()
+    }
+
+    @Command(name = "refresh", arguments = ["[id:string]"], clearance = Clearance.BOT_MANAGER)
+    fun refreshFeed(context: Context, cmdContext: CommandContext) {
+        if (!cmdContext.has("id")) {
+            FeedTask.checkFeeds(context.guild)
+            context.send().success("Refreshed RSS feeds").queue()
+        } else {
+            val id = cmdContext.get<String>("id")!!
+
+            val feed = Model.first(RssFeed::class.java, id)
+
+            if (feed == null || feed.serverId != context.guild.id)
+                throw CommandException("That feed does not exist")
+
+            FeedTask.checkFeed(feed)
+            context.send().success("Refreshed RSS feed").queue()
         }
     }
+
 }

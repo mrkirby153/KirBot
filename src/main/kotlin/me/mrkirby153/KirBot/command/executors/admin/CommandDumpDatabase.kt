6fa8f05ff4c97f33cf94fc4897e01ea425d1d74d
mrkirby153 <mr.austinwhyte@gmail.com>
2018-04-09 01:48:55 -0700

    Compress the output of the database

--- src/main/kotlin/me/mrkirby153/KirBot/command/executors/admin/CommandDumpDatabase.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/command/executors/admin/CommandDumpDatabase.kt
@@ -11,8 +11,10 @@ import me.mrkirby153.KirBot.utils.Context
 import me.mrkirby153.KirBot.utils.nameAndDiscrim
 import me.mrkirby153.kcutils.Time
 import java.io.File
-import java.io.PrintStream
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 @Command(name = "db-backup", clearance = CLEARANCE_GLOBAL_ADMIN)
 class CommandDumpDatabase : BaseCommand(false) {
@@ -29,24 +31,28 @@ class CommandDumpDatabase : BaseCommand(false) {
 
             val s = "mysqldump-${SimpleDateFormat(Time.DATE_FORMAT_NOW).format(
                     System.currentTimeMillis())}"
-            val out = File.createTempFile(s, ".sql")
 
             val proc = ProcessBuilder(dumpCommand.split(" ")).apply { redirectErrorStream(true) }.start()
-            val ps = PrintStream(out)
             val inputStream = proc.inputStream
+
+            val zip = File.createTempFile("mysql", ".sql.zip")
+            val zos = ZipOutputStream(FileOutputStream(zip))
+            val e = ZipEntry("$s.sql")
+            zos.putNextEntry(e)
 
             Bot.LOG.debug("Executing: $dumpCommand")
             while (true) {
                 val read = inputStream.read()
                 if (read == -1)
                     break
-                ps.write(read)
+                zos.write(read)
             }
 
-            ps.flush()
+            zos.closeEntry()
+            zos.close()
 
-            ModuleManager[AdminControl::class.java].logChannel?.sendFile(out, "$s.sql")?.queue{
-                out.delete() // Delete the file after we've sent it
+            ModuleManager[AdminControl::class.java].logChannel?.sendFile(zip, "$s.sql.zip")?.queue{
+                zip.delete() // Delete the file after we've sent it
             }
         } catch (e: Exception) {
            ModuleManager[AdminControl::class.java].logChannel?.sendMessage(":rotating_light: Database backup failed! `${e.message}`")?.queue()

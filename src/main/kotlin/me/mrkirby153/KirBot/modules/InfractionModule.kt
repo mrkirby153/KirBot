f149b50b0a12d331c558aa4631af4ae628a4e807
mrkirby153 <mr.austinwhyte@gmail.com>
2018-05-20 16:03:52 -0700

    Correctly attribute manual bans when adding infractions

--- src/main/kotlin/me/mrkirby153/KirBot/modules/InfractionModule.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/modules/InfractionModule.kt
@@ -3,7 +3,13 @@ package me.mrkirby153.KirBot.modules
 import me.mrkirby153.KirBot.Bot
 import me.mrkirby153.KirBot.infraction.Infraction
 import me.mrkirby153.KirBot.infraction.InfractionType
+import me.mrkirby153.KirBot.logger.LogEvent
 import me.mrkirby153.KirBot.module.Module
+import me.mrkirby153.KirBot.utils.kirbotGuild
+import me.mrkirby153.KirBot.utils.nameAndDiscrim
+import net.dv8tion.jda.core.audit.ActionType
+import net.dv8tion.jda.core.entities.Guild
+import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.guild.GuildBanEvent
 import java.sql.Timestamp
 
@@ -25,7 +31,8 @@ class InfractionModule : Module("infractions") {
         event.guild.banList.queue { banList ->
             val entry = banList.firstOrNull { it.user.id == event.user.id } ?: return@queue
             val infraction = Infraction()
-            infraction.issuerId = null
+            val actor = findBannedUser(event.guild, event.user.id)
+            infraction.issuerId = actor?.id
             infraction.userId = event.user.id
             infraction.guild = event.guild.id
             infraction.createdAt = Timestamp(System.currentTimeMillis())
@@ -33,6 +40,23 @@ class InfractionModule : Module("infractions") {
             infraction.reason = entry.reason ?: "No reason specified"
             infraction.create()
             Bot.LOG.debug("Created infraction ${infraction.id}")
+            event.guild.kirbotGuild.logManager.genericLog(LogEvent.USER_BAN, ":rotating_light:",
+                    "${event.user.nameAndDiscrim} (`${event.user.id}`) was banned by **${actor?.nameAndDiscrim
+                            ?: "Unknown"}** (`${entry.reason}`)")
         }
+    }
+
+    private fun findBannedUser(guild: Guild, user: String): User? {
+        Bot.LOG.debug("Looking up ban for $user in $guild audit logs")
+        var foundUser: User? = null
+        val entries = guild.auditLogs.type(ActionType.BAN).complete()
+        entries.forEach { e ->
+            Bot.LOG.debug("Found ban for ${e.targetId}")
+            if (e.targetId == user && foundUser == null) {
+                foundUser = e.user
+            }
+        }
+        Bot.LOG.debug("Found responsible person: $foundUser")
+        return foundUser
     }
 }

ac4426825b2c9e388163a26d651861b11e4c6cce
mrkirby153 <mr.austinwhyte@gmail.com>
2018-06-17 01:28:32 -0700

    Fix race condition that would cause streams to fail sometimes

--- src/main/kotlin/me/mrkirby153/KirBot/music/AudioPlayerSendHandler.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/music/AudioPlayerSendHandler.kt
@@ -4,11 +4,17 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
 import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame
 import net.dv8tion.jda.core.audio.AudioSendHandler
 
-class AudioPlayerSendHandler(val audioPlayer: AudioPlayer): AudioSendHandler {
+class AudioPlayerSendHandler(val audioPlayer: AudioPlayer) : AudioSendHandler {
 
-    private var lastFrame: AudioFrame? =  null
+    private var lastFrame: AudioFrame? = null
 
-    override fun provide20MsAudio(): ByteArray = lastFrame!!.data;
+    override fun provide20MsAudio(): ByteArray? {
+        if (lastFrame == null)
+            lastFrame = audioPlayer.provide()
+        val data = lastFrame?.data
+        lastFrame = null
+        return data
+    }
 
     override fun canProvide(): Boolean {
         lastFrame = audioPlayer.provide()

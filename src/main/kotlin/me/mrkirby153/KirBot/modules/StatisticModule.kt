11ba640116d5423d7c1f7ed72c7c7a17d4d05311
mrkirby153 <mr.austinwhyte@gmail.com>
2019-05-02 09:56:42 -0700

    More statistics

--- src/main/kotlin/me/mrkirby153/KirBot/modules/StatisticModule.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/modules/StatisticModule.kt
@@ -4,7 +4,10 @@ import me.mrkirby153.KirBot.Bot
 import me.mrkirby153.KirBot.event.Subscribe
 import me.mrkirby153.KirBot.module.Module
 import me.mrkirby153.KirBot.stats.Statistics
+import me.mrkirby153.KirBot.utils.getOnlineStats
+import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
+import java.util.stream.Collectors
 
 class StatisticModule : Module("stats") {
     override fun onLoad() {
@@ -14,18 +17,27 @@ class StatisticModule : Module("stats") {
     @Periodic(1)
     fun updateUsers() {
         // Users
-        val userCount = Bot.shardManager.shards.flatMap { it.users }.filter { !it.isBot }.count()
-        Statistics.userCount.set(userCount.toDouble())
+        val users = Bot.shardManager.shards.flatMap { it.users }.filter { !it.isBot }.stream().collect(
+                Collectors.groupingBy(User::getOnlineStats))
+        users.forEach { status, users ->
+            Statistics.userCount.labels(status.key).set(users.size.toDouble())
+        }
         val botCount = Bot.shardManager.shards.flatMap { it.users }.filter { it.isBot }.count()
         Statistics.botCount.set(botCount.toDouble())
+        Statistics.guilds.set(Bot.shardManager.shards.flatMap { it.guilds }.count().toDouble())
+
+        Bot.shardManager.shards.forEach { shard ->
+            val shardNumber = shard.shardInfo?.shardId ?: 0
+            Statistics.websocketPing.labels(shardNumber.toString()).set(shard.ping.toDouble())
+        }
     }
 
 
     @Subscribe
     fun messageReceived(event: GuildMessageReceivedEvent) {
         when {
-            event.author == event.jda.selfUser -> Statistics.ownMessages.inc()
-            event.author.isBot -> Statistics.botMessages.inc()
+            event.author == event.jda.selfUser -> Statistics.ownMessages.labels(event.guild.id).inc()
+            event.author.isBot -> Statistics.botMessages.labels(event.guild.id).inc()
             else -> Statistics.messages.labels(event.guild.id).inc()
         }
     }
--- src/main/kotlin/me/mrkirby153/KirBot/stats/Statistics.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/stats/Statistics.kt
@@ -13,22 +13,27 @@ object Statistics {
 
     val commandsRan = Counter.build().name("commands_ran").help(
             "How many commands were ran").labelNames("command_name", "guild").register()
-    val messages = Counter.build().name("messages_sent").help(
-            "How many messages were sent").labelNames("guild").register()
-    val ownMessages = Counter.build().name("own_messages").help(
+    val messages = Counter.build().name("messages_received").help(
+            "How many messages were received").labelNames("guild").register()
+    val ownMessages = Counter.build().name("own_messages").labelNames("guild").help(
             "How many of our own messages were sent").register()
-    val botMessages = Counter.build().name("bot_messages").help(
+    val botMessages = Counter.build().name("bot_messages").labelNames("guild").help(
             "How many messages by bots we have seen").register()
 
-    val userCount = Gauge.build().name("bot_users").help("How many users we've seen").register()
-    val botCount = Gauge.build().name("bot_bots").help("How many bots we've seen").register()
+    val userCount = Gauge.build().name("bot_users").labelNames("status").help(
+            "How many users we've seen").register()
+    val guilds = Gauge.build().name("bot_guilds").help("Amount of guilds the bot is in").register()
 
+    val botCount = Gauge.build().name("bot_bots").help("How many bots we've seen").register()
     val commandDuration = Histogram.build().name("bot_command_timing").help(
             "How long commands take to process").labelNames("command").register()
     val eventDuration = Histogram.build().name("bot_event_duration").help(
             "How long events take to process").labelNames("event").register()
     val eventType = Counter.build().name("bot_events").help(
             "The different types of events recevied").labelNames("type").register()
+
+    val websocketPing = Gauge.build().name("websocket_ping").help(
+            "Current websocket ping").labelNames("shard").register()
 
     init {
         DefaultExports.initialize()

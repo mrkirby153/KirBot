85f8bb1a25d70c0009e615511468fe3b7fe69f32
mrkirby153 <mr.austinwhyte@gmail.com>
2020-04-17 21:21:36 -0700

    Ignore bots/webhooks in commands

--- src/main/kotlin/me/mrkirby153/KirBot/modules/Commands.kt
+++ src/main/kotlin/me/mrkirby153/KirBot/modules/Commands.kt
@@ -4,6 +4,7 @@ import me.mrkirby153.KirBot.command.CommandExecutor
 import me.mrkirby153.KirBot.event.Subscribe
 import me.mrkirby153.KirBot.module.Module
 import me.mrkirby153.KirBot.utils.Context
+import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent
 import javax.inject.Inject
@@ -17,9 +18,13 @@ class Commands @Inject constructor(private val commandExecutor: CommandExecutor)
 
     @Subscribe
     fun onMessageReceived(event: MessageReceivedEvent) {
-        if (event.author == event.jda.selfUser)
+        if (event.author == event.jda.selfUser || event.isWebhookMessage || event.author.isBot)
             return
-
+        val chan = event.channel
+        if(chan is TextChannel && !chan.canTalk()) {
+            // Ignore commands where we're unable to respond
+            return
+        }
         val context = Context(event)
 
         commandExecutor.executeAsync(context)
@@ -31,7 +36,7 @@ class Commands @Inject constructor(private val commandExecutor: CommandExecutor)
         if (event.channel.hasLatestMessage())
             if (event.channel.latestMessageId == event.messageId) {
                 // Only process if it's the last message
-                if (event.author == event.jda.selfUser)
+                if (event.author == event.jda.selfUser || event.author.isBot || event.author.isFake)
                     return
 
                 val context = Context(event.message)

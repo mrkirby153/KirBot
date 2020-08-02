f6660e4feffb9c9d011d7003eaf8da0551d56bff
mrkirby153 <mr.austinwhyte@gmail.com>
2020-08-01 17:55:13 -0700

    Add remaining moderation commands

--- src/main/kotlin/com/mrkirby153/kirbot/command/mod/ModerationCommands.kt
+++ src/main/kotlin/com/mrkirby153/kirbot/command/mod/ModerationCommands.kt
@@ -9,11 +9,14 @@ import com.mrkirby153.kirbot.services.command.context.CurrentChannel
 import com.mrkirby153.kirbot.services.command.context.CurrentGuild
 import com.mrkirby153.kirbot.services.command.context.Optional
 import com.mrkirby153.kirbot.services.command.context.Parameter
+import com.mrkirby153.kirbot.services.command.context.Single
 import com.mrkirby153.kirbot.utils.getMember
 import com.mrkirby153.kirbot.utils.nameAndDiscrim
 import com.mrkirby153.kirbot.utils.responseBuilder
+import me.mrkirby153.kcutils.Time
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
+import java.util.concurrent.TimeUnit
 
 @Commands
 class ModerationCommands(val infractionService: InfractionService) {
@@ -26,28 +29,194 @@ class ModerationCommands(val infractionService: InfractionService) {
         }
     }
 
-    @Command(name = "kick", clearance = 100L, userPermissions = [Permission.KICK_MEMBERS],
+    @Command(name = "kick", clearance = 50L, userPermissions = [Permission.KICK_MEMBERS],
             permissions = [Permission.KICK_MEMBERS], category = "moderation")
     fun kick(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
-             @Parameter("user") user: Member,
+             @Parameter("user") member: Member,
              @Parameter("reason") @Optional reason: String?) {
-        if (!guild.selfMember.canInteract(user)) {
+        if (!guild.selfMember.canInteract(member)) {
             throw CommandException(
-                    "I can't kick this user. Ensure that my highest role is above theirs")
+                    "I can't kick this user. Ensure that my highest role is above their highest role")
         }
         val sendMember = sender.getMember(guild) ?: throw CommandException(
                 "You're not a member of this guild")
-        if (!sendMember.canInteract(user)) {
+        if (!sendMember.canInteract(member)) {
             throw CommandException("You can't kick this user")
         }
 
-        infractionService.kick(InfractionService.InfractionContext(user.user, guild, sender,
+        infractionService.kick(InfractionService.InfractionContext(member.user, guild, sender,
                 reason)).handle { result, throwable ->
             if (throwable != null) {
                 throw CommandException(throwable.message ?: "An unknown error occurred")
             }
             channel.responseBuilder.success(buildString {
-                append("Kicked ${user.nameAndDiscrim} {{(`${user.id}`)}}")
+                append("Kicked ${member.nameAndDiscrim} {{(`${member.id}`)}}")
+                if (reason != null) {
+                    append(": {{`$reason`}}")
+                }
+                if (result.infraction != null) {
+                    append(" (#${result.infraction.id})")
+                }
+                append(" {{${getDmResultMessage(result)}}}")
+            })?.queue()
+        }
+    }
+
+    @Command(name = "ban", clearance = 50L, userPermissions = [Permission.BAN_MEMBERS],
+            permissions = [Permission.BAN_MEMBERS], category = "moderation")
+    fun ban(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
+            @Parameter("user") member: Member, @Parameter("reason") @Optional reason: String?) {
+        if (!guild.selfMember.canInteract(member))
+            throw CommandException(
+                    "I can't ban this user. Ensure my highest role is above their highest role")
+        val sendMember = sender.getMember(guild) ?: throw CommandException(
+                "You're not a member of this guild")
+        if (!sendMember.canInteract(member)) {
+            throw CommandException("You can't ban this user")
+        }
+
+        infractionService.ban(InfractionService.InfractionContext(member.user, guild, sender,
+                reason)).handle { result, throwable ->
+            if (throwable != null) {
+                throw CommandException(throwable.message ?: "An unknown error occurred")
+            }
+            channel.responseBuilder.success(buildString {
+                append("Banned ${member.nameAndDiscrim} {{(`${member.id}`)}}")
+                if (reason != null) {
+                    append(": {{`$reason`}}")
+                }
+                if (result.infraction != null) {
+                    append(" (#${result.infraction.id})")
+                }
+                append(" {{${getDmResultMessage(result)}}}")
+            })?.queue()
+        }
+    }
+
+    @Command(name = "mute", clearance = 50L, userPermissions = [Permission.MANAGE_ROLES],
+            permissions = [Permission.MANAGE_ROLES], category = "moderation")
+    fun mute(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
+             @Parameter("user") member: Member, @Parameter("reason") @Optional reason: String?) {
+        if (!guild.selfMember.canInteract(member))
+            throw CommandException(
+                    "I can't mute this user. Ensure my highest role is above their highest role")
+        val sendMember = sender.getMember(guild) ?: throw CommandException(
+                "You're not a member of this guild")
+        if (!sendMember.canInteract(member)) {
+            throw CommandException("You can't mute this user")
+        }
+
+        infractionService.mute(InfractionService.InfractionContext(member.user, guild, sender,
+                reason)).handle { result, throwable ->
+            if (throwable != null) {
+                throw CommandException(throwable.message ?: "An unknown error occurred")
+            }
+            channel.responseBuilder.success(buildString {
+                append("Muted ${member.nameAndDiscrim} {{(`${member.id}`)}}")
+                if (reason != null) {
+                    append(": {{`$reason`}}")
+                }
+                if (result.infraction != null) {
+                    append(" (#${result.infraction.id})")
+                }
+                append(" {{${getDmResultMessage(result)}}}")
+            })?.queue()
+        }
+    }
+
+    @Command(name = "warn", clearance = 50L, userPermissions = [Permission.KICK_MEMBERS],
+            category = "moderation")
+    fun warn(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
+             @Parameter("user") member: Member, @Parameter("reason") @Optional reason: String?) {
+        val sendMember = sender.getMember(guild) ?: throw CommandException(
+                "You're not a member of this guild")
+        if (!sendMember.canInteract(member)) {
+            throw CommandException("You can't warn this user")
+        }
+
+        infractionService.warn(InfractionService.InfractionContext(member.user, guild, sender,
+                reason)).handle { result, throwable ->
+            if (throwable != null) {
+                throw CommandException(throwable.message ?: "An unknown error occurred")
+            }
+            channel.responseBuilder.success(buildString {
+                append("Warned ${member.nameAndDiscrim} {{(`${member.id}`)}}")
+                if (reason != null) {
+                    append(": {{`$reason`}}")
+                }
+                if (result.infraction != null) {
+                    append(" (#${result.infraction.id})")
+                }
+                append(" {{${getDmResultMessage(result)}}}")
+            })?.queue()
+        }
+    }
+
+    @Command(name = "tempmute", clearance = 50L, userPermissions = [Permission.MANAGE_ROLES],
+            permissions = [Permission.MANAGE_ROLES], category = "moderation")
+    fun tempmute(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
+                 @Parameter("user") member: Member, @Parameter("duration") @Single duration: String,
+                 @Parameter("reason") @Optional reason: String?) {
+        if (!guild.selfMember.canInteract(member))
+            throw CommandException(
+                    "I can't mute this user. Ensure my highest role is above their highest role")
+        val sendMember = sender.getMember(guild) ?: throw CommandException(
+                "You're not a member of this guild")
+        if (!sendMember.canInteract(member)) {
+            throw CommandException("You can't mute this user")
+        }
+
+        val durationMs = try {
+            Time.parse(duration)
+        } catch (e: IllegalArgumentException) {
+            throw CommandException(e.message ?: "An unknown error occurred")
+        }
+
+        infractionService.tempMute(InfractionService.InfractionContext(member.user, guild, sender,
+                reason), durationMs, TimeUnit.MILLISECONDS).handle { result, throwable ->
+            if (throwable != null) {
+                throw CommandException(throwable.message ?: "An unknown error occurred")
+            }
+            channel.responseBuilder.success(buildString {
+                append("Temporarily muted ${member.nameAndDiscrim} {{(`${member.id}`)}} for ${Time.format(1, durationMs, smallest = Time.TimeUnit.SECONDS)}")
+                if (reason != null) {
+                    append(": {{`$reason`}}")
+                }
+                if (result.infraction != null) {
+                    append(" (#${result.infraction.id})")
+                }
+                append(" {{${getDmResultMessage(result)}}}")
+            })?.queue()
+        }
+    }
+
+    @Command(name = "tempban", clearance = 50L, userPermissions = [Permission.BAN_MEMBERS],
+            permissions = [Permission.BAN_MEMBERS], category = "moderation")
+    fun tempban(sender: CommandSender, guild: CurrentGuild, channel: CurrentChannel,
+                @Parameter("user") member: Member, @Parameter("duration") @Single duration: String,
+                @Parameter("reason") @Optional reason: String?) {
+        if (!guild.selfMember.canInteract(member))
+            throw CommandException(
+                    "I can't ban this user. Ensure my highest role is above their highest role")
+        val sendMember = sender.getMember(guild) ?: throw CommandException(
+                "You're not a member of this guild")
+        if (!sendMember.canInteract(member)) {
+            throw CommandException("You can't ban this user")
+        }
+
+        val durationMs = try {
+            Time.parse(duration)
+        } catch (e: IllegalArgumentException) {
+            throw CommandException(e.message ?: "An unknown error occurred")
+        }
+
+        infractionService.tempBan(InfractionService.InfractionContext(member.user, guild, sender,
+                reason), durationMs, TimeUnit.MILLISECONDS).handle { result, throwable ->
+            if (throwable != null) {
+                throw CommandException(throwable.message ?: "An unknown error occurred")
+            }
+            channel.responseBuilder.success(buildString {
+                append("Temporarily banned ${member.nameAndDiscrim} {{(`${member.id}`)}} for ${Time.format(1, durationMs, smallest = Time.TimeUnit.SECONDS)}")
                 if (reason != null) {
                     append(": {{`$reason`}}")
                 }
